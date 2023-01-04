38eff28a9cd76a233a88b0f8661f2680102cf38f
Ryan <49217984+sRemorse@users.noreply.github.com>
2023-01-04 21:53:03 +0000

    Add the ability to target others
    Can now target the god command towards other players if you have the command

--- src/main/java/com/remorse/remorseutils/Commands/GodCommand/GodCommand.java
+++ src/main/java/com/remorse/remorseutils/Commands/GodCommand/GodCommand.java
@@ -1,21 +1,45 @@
 package com.remorse.remorseutils.Commands.GodCommand;
 
+import com.remorse.remorseutils.RemorseUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+
 public class GodCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
+        // God on yourself
         if(sender instanceof Player player) {
-            if (!player.hasPermission("remorseutils.god")) return false;
-            if (args.length != 0) return false;
-            toggleGod(player);
+            if (!player.hasPermission("remorseutils.god")){
+                sender.sendMessage(RemorseUtils.translate("&c[RemorseUtils] You don't have permission to do this!"));
+                return true;
+            }
+            if (args.length == 0) {
+                toggleGod(player);
+                return true;
+            }
         }
+
+            // God on target
+            if (!sender.hasPermission("remorseutils.god.others")) {
+                sender.sendMessage(RemorseUtils.translate("&c[RemorseUtils] You don't have permission to do this to that player!"));
+                return true;
+            }
+                if (args.length > 0) {
+                    Player target = Bukkit.getPlayer(args[0]);
+                    if(target == null){
+                        sender.sendMessage(RemorseUtils.translate("&c[RemorseUtils] User is not online"));
+                        return true;
+                    }
+                    toggleGod(target);
+                    return true;
+                }
         return true;
     }
 
@@ -23,10 +47,11 @@ public class GodCommand implements CommandExecutor {
         String name = player.getName();
         if(!GodModeHandler.getGodPlayers().contains(name)){
             GodModeHandler.addPlayer(name);
-            player.sendMessage(ChatColor.translateAlternateColorCodes('&',"&6[RemorseUtils] &9Godmode enabled"));
+            player.setHealth(20.0); // Heal player to max health when adding them to the GodPlayers list
+            player.sendMessage(RemorseUtils.translate("&6[RemorseUtils] &aGodmode enabled"));
         } else {
             GodModeHandler.removePlayer(name);
-            player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&6[RemorseUtils] &9Godmode disabled"));
+            player.sendMessage(RemorseUtils.translate("&6[RemorseUtils] &cGodmode disabled"));
         }
     }
 
