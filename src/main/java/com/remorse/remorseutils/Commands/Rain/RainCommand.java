f278339eabc4e49d5bcc4b55b9c3984a251ad600
Ryan <49217984+sRemorse@users.noreply.github.com>
2023-01-07 01:35:57 +0000

    Create rain command class
    Toggle-able rain command works by getting the state of the current weather and switching it
    Not available to console

---
+++ src/main/java/com/remorse/remorseutils/Commands/Rain/RainCommand.java
@@ -0,0 +1,40 @@
+package com.remorse.remorseutils.Commands.Rain;
+
+import com.remorse.remorseutils.RemorseUtils;
+import org.bukkit.WeatherType;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class RainCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if(!(sender instanceof Player player)){
+            sender.sendMessage(RemorseUtils.translate("&c[RemorseUtils] Console cannot do this"));
+            return true;
+        }
+
+        if (player.hasPermission("remorseutils.rain")) {
+            if (args.length == 0) {
+                if (player.getWorld().isClearWeather()) {
+                    player.getWorld().setStorm(true);
+                    player.sendMessage(RemorseUtils.translate("&6[RemorseUtils] &bRain enabled"));
+                } else {
+                    player.getWorld().setStorm(false);
+                    player.getWorld().setThundering(false);
+                    player.getWorld().setClearWeatherDuration(0);
+                    player.sendMessage(RemorseUtils.translate("&6[RemorseUtils] &7Rain disabled"));
+                }
+            }
+            return true;
+        }
+        player.sendMessage(RemorseUtils.translate("&c[RemorseUtils] You don't have permission to do this!"));
+        return true;
+
+    }
+}
+
+
